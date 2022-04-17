abec309bb9c960b6ad2d3f3ccfb8bb0d1aa36440
Ваньк Ваньк <praktik.valera@yandex.ru>
2022-04-17 22:24:46 +0700

    servxxx

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -140,11 +140,25 @@ class RecipeSerializerCreate(serializers.ModelSerializer):
             raise serializers.ValidationError('Время должно быть больше 0')
         return data
 
+    def create_ingredients(self, ingredients, recipe):
+        for ingredient in ingredients:
+            RecipeIngredient.objects.create(
+                recipe=recipe,
+                ingredient_id=ingredient.get('id'),
+                amount=ingredient.get('amount'), )
+
     def create(self, validated_data):
-        ingredients = validated_data.pop('recipe_ingredient')
+        ingredients = validated_data.pop('ingredients')
         tags = validated_data.pop('tags')
         recipe = Recipe.objects.create(**validated_data)
-        return self.tags_ingredients(ingredients, tags, recipe)
+        recipe.tags.set(tags)
+        self.create_ingredients(ingredients, recipe)
+        return recipe
+    # def create(self, validated_data):
+    #     ingredients = validated_data.pop('recipe_ingredient')
+    #     tags = validated_data.pop('tags')
+    #     recipe = Recipe.objects.create(**validated_data)
+    #     return self.tags_ingredients(ingredients, tags, recipe)
 
     def update(self, instance, validated_data):
         ingredients = validated_data.pop('recipe_ingredient')
