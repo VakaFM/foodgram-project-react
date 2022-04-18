97978e92a890e5549d0fa7ed283eb4fd75a91c6d
Ваньк Ваньк <praktik.valera@yandex.ru>
2022-04-18 17:05:12 +0700

    servxxx

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -224,27 +224,26 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class FavoriteSerializer(serializers.ModelSerializer):
-    recipe = serializers.PrimaryKeyRelatedField(queryset=Recipe.objects.all())
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    id = serializers.IntegerField(source='recipe.id', read_only=True)
+    name = serializers.CharField(source='recipe.name', read_only=True)
+    cooking_time = serializers.CharField(source='recipe.cooking_time',
+                                         read_only=True)
+    image = serializers.CharField(source='recipe.image', read_only=True)
+
+    def validated(self, data):
+        request = self.context.get('request')
+        if request.method == 'DELETE':
+            return data
+        recipe_id = request.parser_context.get('kwargs').get('recipe_id')
+        recipe = get_object_or_404(Recipe, id=recipe_id)
+        user = self.context['request'].user
+        if Favorite.objects.filter(recipe=recipe, user=user).exists():
+            raise serializers.ValidationError('Такой рецепт уже добавлен')
+        return data
 
     class Meta:
         model = Favorite
-        fields = ('user', 'recipe')
-
-    def validate(self, data):
-        request = self.context.get('request')
-        recipe_id = data['recipe'].id
-        favorite_exists = Favorite.objects.filter(
-            user=request.user,
-            recipe__id=recipe_id
-        ).exists()
-
-        if request.method == 'POST' and favorite_exists:
-            raise serializers.ValidationError(
-                'Рецепт уже добавлен в избранное'
-            )
-
-        return data
+        fields = ('id', 'name', 'image', 'cooking_time',)
 
 
 class ShoppingCartSerializer(serializers.ModelSerializer):
