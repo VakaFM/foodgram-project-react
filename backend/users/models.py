6bceec99e0f7040ea2ae0f337bc6198b751bae35
Ваньк Ваньк <praktik.valera@yandex.ru>
2022-05-12 14:29:12 +0700

    rrt

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.email
