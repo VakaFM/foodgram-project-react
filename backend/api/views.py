a6f248cef35c425015485d2798b680b70f8576b7
Ваньк Ваньк <praktik.valera@yandex.ru>
2022-05-13 18:15:54 +0700

    u

--- backend/api/views.py
+++ backend/api/views.py
@@ -57,25 +57,33 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
     @action(
-        methods=['GET', ],
-        url_path='shopping_cart',
-        detail=False,
-    )
+        detail=False, methods=['get'], permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
-        ingredients = RecipeIngredient.objects.filter(
-            recipe__shopping_cart__user=request.user
-        ).values(
-            'ingredients__name',
-            'ingredients__measurement_unit'
-        ).annotate(total_amount=Sum('amount'))
-        shopping_list = ['{} ({}) - {}\n'.format(
-            ingredient['ingredients__name'],
-            ingredient['ingredients__measurement_unit'],
-            ingredient['total_amount']
-        ) for ingredient in ingredients]
+        user = get_object_or_404(User, username=request.user.username)
+        shopping_cart = user.cart.all()
+        shopping_dict = {}
+        for num in shopping_cart:
+            ingredients_queryset = num.recipe.ingredient.all()
+            for ingredient in ingredients_queryset:
+                name = ingredient.ingredients.name
+                amount = ingredient.amount
+                measurement_unit = ingredient.ingredients.measurement_unit
+                if name not in shopping_dict:
+                    shopping_dict[name] = {
+                        'measurement_unit': measurement_unit,
+                        'amount': amount}
+                else:
+                    shopping_dict[name]['amount'] = (
+                        shopping_dict[name]['amount'] + amount)
+
+        shopping_list = []
+        for index, key in enumerate(shopping_dict, start=1):
+            shopping_list.append(
+                f'{index}. {key} - {shopping_dict[key]["amount"]} '
+                f'{shopping_dict[key]["measurement_unit"]}\n')
+        filename = 'shopping_cart.txt'
         response = HttpResponse(shopping_list, content_type='text/plain')
-        attachment = 'attachment; filename="shopping_cart.txt"'
-        response['Content-Disposition'] = attachment
+        response['Content-Disposition'] = f'attachment; filename={filename}'
         return response
 
 
